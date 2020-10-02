974084b150faa0beaaa3323e39b67bd26164e2c1
Rizki Afandi <r.afandi.21@students.unnes.ac.id>
2020-10-02 18:30:49 +0700

    update

--- tools/footer.php
+++ tools/footer.php
@@ -1,60 +1,62 @@
 <footer class="footer-area section-gap">
-					<div class="container">
-						<div class="row">
-	<div class="col-lg-6 col-md-6 col-sm-6">
-			<ul class="single-footer-widget">
-				<li><a href="index.html">Beranda</a></li>
-							<li><a href="about.php">Tentang</a></li>
-							<li><a href="cars.php">Mobil</a></li>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6">
+				<ul class="single-footer-widget">
+					<li><a href="index.html">Beranda</a></li>
+					<li><a href="about.php">Tentang</a></li>
+					<li><a href="cars.php">Mobil</a></li>
 
-							<li><a href="contact.php">Kontak</a></li>
-													
-			</ul>
-							</div>												
-							<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
-								<div class="single-footer-widget">
-									<h6>&nbsp;Kontak Kami</h6>
-									
-									<div class="footer-social d-flex align-items-center">
-										
-										<a href="https://wa.me/6281228547149?text=Butuh%20bantuan"><i class="fa fa-whatsapp"></i></a>
-										<a href="https://www.instagram.com/win_gp/"><i class="fa fa-instagram"></i></a>
-									</div>
-								</div>
-							</div>							
-							<div class="col-lg-4  col-md-6 col-sm-6">
-								<div class="single-footer-widget">
-									<h6>&nbsp;TENTANG HALAMAN</h6>
-									<p>website ini hadir untuk memudahkan anda dalam menyewa mobil dan menjangkau seluruh pelosok desa.</p>
-	</div>
-							</div>	
-							<p class="mt-50 mx-auto footer-text col-lg-12">
-								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	Copyright &copy;<script>document.write(new Date().getFullYear());</script> Gabriela Trans
-							</p>											
-						</div>
+					<li><a href="contact.php">Kontak</a></li>
+
+				</ul>
+			</div>
+			<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
+				<div class="single-footer-widget">
+					<h6>&nbsp;Kontak Kami</h6>
+
+					<div class="footer-social d-flex align-items-center">
+
+						<a href="https://wa.me/6281228547149?text=Butuh%20bantuan"><i class="fa fa-whatsapp"></i></a>
+						<a href="https://www.instagram.com/win_gp/"><i class="fa fa-instagram"></i></a>
 					</div>
+				</div>
+			</div>
+			<div class="col-lg-4  col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h6>&nbsp;TENTANG HALAMAN</h6>
+					<p>website ini hadir untuk memudahkan anda dalam menyewa mobil dan menjangkau seluruh pelosok desa.</p>
+				</div>
+			</div>
+			<p class="mt-50 mx-auto footer-text col-lg-12">
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				Copyright &copy;<script>
+					document.write(new Date().getFullYear());
+				</script> Gabriela Trans
+			</p>
+		</div>
+	</div>
 
-				</footer>	
-				<!-- End footer Area -->		
+</footer>
+<!-- End footer Area -->
 
-				<script src="js/vendor/jquery-2.2.4.min.js"></script>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-				<script src="js/vendor/bootstrap.min.js"></script>			
-				<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-				<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>			
-				<script src="js/easing.min.js"></script>			
-				<script src="js/hoverIntent.js"></script>
-				<script src="js/superfish.min.js"></script>	
-				<script src="js/jquery.ajaxchimp.min.js"></script>
-				<script src="js/jquery.magnific-popup.min.js"></script>	
-				<script src="js/owl.carousel.min.js"></script>			
-				<script src="js/jquery.sticky.js"></script>
-				<script src="js/jquery.nice-select.min.js"></script>	
-				<script src="js/waypoints.min.js"></script>
-				<script src="js/jquery.counterup.min.js"></script>					
-				<script src="js/parallax.min.js"></script>		
-				<script src="js/mail-script.js"></script>	
-				<script src="js/main.js"></script>
-				<script src="js/vendor/bootstrap.min.js"></script>
-				<script src="js/jquery.timepicker.min.js"></script>
+<script src="js/vendor/jquery-2.2.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="js/easing.min.js"></script>
+<script src="js/hoverIntent.js"></script>
+<script src="js/superfish.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/main.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script src="js/jquery.timepicker.min.js"></script>
--- tools/master_mobil.php
+++ tools/master_mobil.php
@@ -1,131 +1,139 @@
 <section class="model-area section-gap" id="cars">
-				  <div class="container">
-				    <div class="row d-flex justify-content-center pb-40">
-				      <div class="col-md-8 pb-40 header-text">
-				        <h1 class="text-center pb-10">Pilih Mobilmu Sekarang !&nbsp;&nbsp;</h1>
-				        <p class="text-center"> Pilihlah Mobil seesuai kebutuhan untuk menghemat peengeluaran</p>
-			          </div>
-			        </div>
-				    <div class="active-model-carusel">
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/AVANZA.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Toyota Avanza&nbsp;</h4>
-				            <h2>Rp.550.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok  disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar  jemput ke bandara / stasiun , wisuda, maupun pullang kampung. </p>
-				          Kapasitas  7 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="avanza.php">PESAN SEKARANG</a></div>
-			          </div>
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/INOVA.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Toyota Inova</h4>
-				            <h2>Rp.750.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
-				          Kapasitas 7 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="inova.php">PESAN SEKARANG </a></div>
-			          </div>
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/PAJERO.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Pajero</h4>
-				            <h2>Rp.1.300.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
-				          Kapasitas 7 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi  anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="pajero.php">PESAN SEKARANG</a></div>
-			          </div>
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/FORTUNER.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Fortuner</h4>
-				            <h2>Rp.1.300.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
-				          Kapasitas 7 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="fortuner.php">PESAN SEKARANG</a></div>
-			          </div>
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/ALPARD.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Alphard</h4>
-				            <h2>Rp.3.000.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
-				          Kapasitas 6 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="alpard.php">PESAN SEKARANG</a></div>
-			          </div>
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/ELF.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Isuzu Elf</h4>
-				            <h2>Rp.1.200.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
-				          Kapasitas 18 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="elf.php">PESAN SEKARANG</a></div>
-			          </div>
-				      <div class="row align-items-center single-model item">
-				        <div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/HIACE.jpg" alt=""></div>
-				        <div class="col-lg-6 model-right">
-				          <div class="title justify-content-between d-flex">
-				            <h4 class="mt-20">Toyota Hiace</h4>
-				            <h2>Rp.1.300.000<span>/hari</span></h2>
-			              </div>
-				          <p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
-				          Kapasitas 16 orang dewasa <br>
-				          <p>
-				          <h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
-				          <h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
-				          <h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
-				          <h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa   0812285417419</em></strong></h6>
-				          </p>
-				          <a class="text-uppercase primary-btn" href="hiace.php">PESAN SEKARANG</a></div>
-			          </div>
-			        </div>
-			      </div>
-		    </section>
+	<div class="container">
+		<div class="row d-flex justify-content-center pb-40">
+			<div class="col-md-8 pb-40 header-text">
+				<h1 class="text-center pb-10">Pilih Mobilmu Sekarang !&nbsp;&nbsp;</h1>
+				<p class="text-center"> Pilihlah Mobil seesuai kebutuhan untuk menghemat peengeluaran</p>
+			</div>
+		</div>
+		<div class="active-model-carusel">
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/AVANZA.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Toyota Avanza&nbsp;</h4>
+						<h2>Rp.550.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar jemput ke bandara / stasiun , wisuda, maupun pullang kampung. </p>
+					Kapasitas 7 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="avanza.php">PESAN SEKARANG</a>
+				</div>
+			</div>
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/INOVA.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Toyota Inova</h4>
+						<h2>Rp.750.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
+					Kapasitas 7 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="inova.php">PESAN SEKARANG </a>
+				</div>
+			</div>
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/PAJERO.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Pajero</h4>
+						<h2>Rp.1.300.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
+					Kapasitas 7 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="pajero.php">PESAN SEKARANG</a>
+				</div>
+			</div>
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/FORTUNER.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Fortuner</h4>
+						<h2>Rp.1.300.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
+					Kapasitas 7 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="fortuner.php">PESAN SEKARANG</a>
+				</div>
+			</div>
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/ALPARD.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Alphard</h4>
+						<h2>Rp.3.000.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
+					Kapasitas 6 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="alpard.php">PESAN SEKARANG</a>
+				</div>
+			</div>
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/ELF.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Isuzu Elf</h4>
+						<h2>Rp.1.200.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
+					Kapasitas 18 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="elf.php">PESAN SEKARANG</a>
+				</div>
+			</div>
+			<div class="row align-items-center single-model item">
+				<div class="col-lg-6 model-left"> <img class="img-fluid" src="img/img/stok/HIACE.jpg" alt=""></div>
+				<div class="col-lg-6 model-right">
+					<div class="title justify-content-between d-flex">
+						<h4 class="mt-20">Toyota Hiace</h4>
+						<h2>Rp.1.300.000<span>/hari</span></h2>
+					</div>
+					<p> Mobil ini cocok untuk disegala aktivitas seperti berangkat ke kantor, wisata keluarga, antar / jemput ke bandara, wisuda,maupun pullang kampung. </p>
+					Kapasitas 16 orang dewasa <br>
+					<p>
+						<h6 class="text-left text-uppercase"><em><strong>Perhatian : </strong></em></h6>
+						<h6 class="text-left">1. Harga termasuk mobil,driver, dan BBM. </h6>
+						<h6 class="text-left">2.Pembelian tiket TOL ditanggung penumpang.&nbsp;&nbsp; </h6>
+						<h6>3.Jika lokasi anda <strong><em>di luar kota semarang</em></strong> ,anda dapat memesan lewat <strong><em>wa 0812285417419</em></strong></h6>
+					</p>
+					<a class="text-uppercase primary-btn" href="hiace.php">PESAN SEKARANG</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
